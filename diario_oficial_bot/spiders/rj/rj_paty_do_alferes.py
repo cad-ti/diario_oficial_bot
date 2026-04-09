20b3299081ee0f76a001011a421757c49bfb8c7b
Gabriel-gag <Gabrielalvesgiuliano@gmail.com>
2026-04-09 11:31:58 -0300

    Update rj_paty_do_alferes.py

--- diario_oficial_bot/spiders/rj/rj_paty_do_alferes.py
+++ diario_oficial_bot/spiders/rj/rj_paty_do_alferes.py
@@ -1,63 +1,67 @@
-import re
-from datetime import date, datetime as dt
-import dateparser 
+from datetime import date
+import scrapy
+from parsel import Selector
 
 from diario_oficial_bot.items import Gazette
 from diario_oficial_bot.spiders.base import BaseGazetteSpider
-
-from scrapy import Request
 
 
 class RjPatyDoAlferesSpider(BaseGazetteSpider):
     name = "rj_paty_do_alferes"
     TERRITORY_ID = "3303856"
     allowed_domains = ["patydoalferes.rj.gov.br"]
-    
-    start_urls = ["https://patydoalferes.rj.gov.br/diario-oficial/"] 
     start_date = date(2009, 1, 1)
 
+    def start_requests(self):
+        yield scrapy.Request(
+            url="https://patydoalferes.rj.gov.br/",
+            callback=self.make_requests_for_years,
+            headers={"User-Agent": "Mozilla/5.0"},
+        )
+
+    def make_requests_for_years(self, response):
+        url = "https://patydoalferes.rj.gov.br/wp-admin/admin-ajax.php"
+
+        for year in range(self.start_date.year, date.today().year + 1):
+            yield scrapy.FormRequest(
+                url=url,
+                formdata={
+                    "action": "pdo_old_list_rows",
+                    "dirrel": str(year),
+                },
+                headers={
+                    "User-Agent": "Mozilla/5.0",
+                    "Referer": "https://patydoalferes.rj.gov.br/",
+                    "X-Requested-With": "XMLHttpRequest",
+                },
+                #cb_kwargs={"year": year}
+            )
+
     def parse(self, response):
+        data = response.json()
+        html = data.get("data", {}).get("html", "")
 
-        all_diario_links = response.xpath("//ul//a[@class='bold' and @title]")
+        selector = Selector(text=html)
 
-        for link in all_diario_links:
+        for tr in selector.xpath("//tr"):
+            url = tr.xpath("./@data-url").get()
+            date_str = tr.xpath("./@data-dateiso").get()
+            edition = tr.xpath("./@data-edicao").get()  
+            complemento = tr.xpath("./td[3]/text()").get()
             
-            title_attr = link.xpath("./@title").get()
-            download_url_relative = link.xpath("./@href").get()
-            
-            edition_match = re.search(r"D\.O\.\s*(\d+)", title_attr)
-            edition_number = edition_match.group(1) if edition_match else None
-            
-            date_match = re.search(r'(\d{1,2}/\d{1,2}/\d{4})', title_attr)
-            raw_date_str = date_match.group(1) if date_match else None
-  
-            try:
-                date_obj = dateparser.parse(
-                    raw_date_str.strip(), 
-                    date_formats=['%d/%m/%Y', '%m/%d/%Y'], 
-                    languages=['pt'] 
-                )
-                
-                
-                gazette_date = date_obj.date()
-                
-            except Exception:
-                continue
-            
+            gazette_date = date.fromisoformat(date_str)
+
             if gazette_date > self.end_date:
                 continue
-            
             if gazette_date < self.start_date:
-                continue 
-
-            file_url = response.urljoin(download_url_relative)
-            is_extra_edition = "Esp" in title_attr or "ESP" in title_attr.upper()
+                return
             
+            is_extra_edition = bool(complemento and complemento.strip())
+            #NAO ESTA FUNCIONANDO O EXTRA EDITION
             yield Gazette(
                 date=gazette_date,
-                edition_number=edition_number,
-                file_urls=[file_url],
+                file_urls=[url],
+                edition_number=edition,
                 is_extra_edition=is_extra_edition,
-                power="executive",
+                power="executive_legislative",
             )
-        
